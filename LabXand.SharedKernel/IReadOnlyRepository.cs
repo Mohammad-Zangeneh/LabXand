49c9351fa16dee50f0fc9041b04e0627678daa4c
Mohammad-Zangeneh <Mohammad.Zangeneh@gmail.com>
2024-03-05 17:45:23 +0330

    Change return type to List

--- LabXand.SharedKernel/IReadOnlyRepository.cs
+++ LabXand.SharedKernel/IReadOnlyRepository.cs
@@ -7,6 +7,6 @@
         Task<int> CountAsync<T>(IQueryable<T> query, CancellationToken cancellationToken) where T : class;
         Task<T?> FirstOrDefaultAsync<T>(IQueryable<T> query, CancellationToken cancellationToken) where T : class;
         Task<(List<T> Items, int TotalCount)> GetPaginatedAsync<T>(IQueryable<T> query, int page, int size, CancellationToken cancellationToken) where T : class;
-        Task<IList<T>> GetListAsync<T>(IQueryable<T> query, CancellationToken cancellationToken) where T : class;
+        Task<List<T>> GetListAsync<T>(IQueryable<T> query, CancellationToken cancellationToken) where T : class;
     }
 }
