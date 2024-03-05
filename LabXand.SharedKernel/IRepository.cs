7a20592fec7734c9885898707dfdb66bbb472745
Mohammad-Zangeneh <Mohammad.Zangeneh@gmail.com>
2024-03-05 17:38:27 +0330

    Refactor IRepository

--- LabXand.SharedKernel/IRepository.cs
+++ LabXand.SharedKernel/IRepository.cs
@@ -1,18 +1,7 @@
-﻿using LabXand.Core;
-
-namespace LabXand.SharedKernel
+﻿namespace LabXand.SharedKernel
 {
-    public interface IReadOnlyRepository<TAggregateRoot>
-        where TAggregateRoot : IAggregateRoot
-    {
-        IQueryable<TAggregateRoot> Query { get; }
-        Task<IList<T>> GetListAsync<T>(IQueryable<T> query);
-        Task<T> GetAsync<T>(IQueryable<T> query);
-        Task<IPagedList<T>> GetPageAsync<T>(IQueryable<T> query, int page, int size);
-
-    }
     public interface IRepository<TAggregateRoot> : IReadOnlyRepository<TAggregateRoot>
-        where TAggregateRoot : IAggregateRoot 
+        where TAggregateRoot : IAggregateRoot
     {
         void Add(TAggregateRoot domain);
         void Edit(TAggregateRoot domain);
