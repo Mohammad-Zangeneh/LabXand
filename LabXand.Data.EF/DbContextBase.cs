f1393a5587e8e96904ba40c58c7cebef57d56ba8
Mohammad-Zangeneh <Mohammad.Zangeneh@gmail.com>
2024-03-18 15:21:26 +0330

    Add Tcontext to DbContextbase constructor

--- LabXand.Data.EF/DbContextBase.cs
+++ LabXand.Data.EF/DbContextBase.cs
@@ -3,7 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LabXand.Data.EF;
 
-public class DbContextBase(DbContextOptions options) : DbContext(options), IUnitOfWork
+public class DbContextBase<TContext>(DbContextOptions<TContext> options) : DbContext(options), IUnitOfWork
+    where TContext : DbContext
 {
     public Task CommitAsync(CancellationToken cancellationToken) => SaveChangesAsync(cancellationToken);
 }
