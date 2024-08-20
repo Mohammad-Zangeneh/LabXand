e8d65cbcd7bd57e73101982d898bf357db75b545
Mohammad Zangeneh <mohammad.zangeneh@gmail.com>
2024-08-20 11:48:32 +0330

    improve event dispatching

---
+++ LabXand.SharedKernel/EventHandlerBase.cs
@@ -0,0 +1,12 @@
+﻿namespace LabXand.SharedKernel;
+
+public abstract class EventHandlerBase<TDomainEvent> : IEventHandler<TDomainEvent>
+    where TDomainEvent : IDomainEvent
+{
+    public abstract bool CanHandle(IDomainEvent domainEvent);
+
+    public abstract Task HandleAsync(TDomainEvent domainEvent, CancellationToken cancellationToken = default);
+
+    public Task HandleAsync(object domainEvent, CancellationToken cancellationToken = default) => HandleAsync((TDomainEvent)domainEvent, cancellationToken);
+    
+}
